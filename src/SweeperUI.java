b081dbfdc85b47f000e507f173ee994fc6b394ba
CurlyGurt <hilliard.garrett31@gmail.com>
2026-04-09 12:56:01 -0500

    Changed SweeperUI to only use libraries absolutely required instead of grabbing entire pieces with *

--- src/SweeperUI.java
+++ src/SweeperUI.java
@@ -1,6 +1,13 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.BorderFactory;
 import javax.swing.border.Border;
 
 
